91e15b5b6c9bd4d2e585d8dd2c7dacaa7cf83346
0x00Crypt <116393553+0x00Crypt@users.noreply.github.com>
2022-10-22 01:21:24 +0300

    best crypt -> https://t.me/Crypt_0x00_V2
    Lets go to buy crypt https://t.me/Crypt_0x00_V2

---
+++ Crypt.cs
@@ -0,0 +1,10 @@
+namespace CryptFile
+{
+    class CryptorFile
+    {
+        void Main()
+        {
+            Console.Writeline("Best cryptor -> https://t.me/Crypt_0x00_V2<-");
+        }
+    }
+}
